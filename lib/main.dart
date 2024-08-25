266349f91f2a12eb1b29c0a743e9d179673e20f0
AltafMohdShaikh <altafmohdshaikh@gmail.com>
2024-08-25 23:24:14 +0530

    clone

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:calculator_flutter/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
